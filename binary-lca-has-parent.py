f2ccc31e2a560ba285127e2c95d464002815dd75
mdl <30521886+mdl@users.noreply.github.com>
2020-12-02 02:55:51 -0600

    Update and rename lca-has-parent.py to binary-lca-has-parent.py

--- binary-lca-has-parent.py
+++ binary-lca-has-parent.py
@@ -10,3 +10,11 @@ def lowestCommonAncestor(self, p, q):
     if node.parent: stack.append(node.parent)
 
   return None
+
+def lowestCommonAncestorZeroSpace(self, p, q):
+  p1, p2 = p, q
+  while p1 != p2:
+    p1 = p1.parent if p1.parent else q
+    p2 = p2.parent if p2.parent else p
+
+  return p1
